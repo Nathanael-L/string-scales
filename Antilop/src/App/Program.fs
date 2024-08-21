fa1bcdd8e0038718c03d5bb0024b720ced320449
Nathanael Lang <nathan@localhost.localdomain>
2024-08-21 11:00:13 +0200

    feat: generate mayor scales for each note

---
+++ Antilop/src/App/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open Library
+open ScalerCore
+
+let printMayorScale (noteName: string) =
+    let note : Note = getNoteFromString noteName
+    let scale = getMayorScale note
+    printfn $"Scale: %0A{scale}"
+
+[<EntryPoint>]
+let main args =
+    printfn "__+---SCALER---+__"
+    printfn "1: Mayor Scale"
+    printfn " Select: "
+    let mode = Console.ReadLine()
+    printfn " Tone: "
+    let note = Console.ReadLine()
+
+    match mode with
+        | "1" -> printMayorScale note
+        | _ -> printfn "unknown input"
+
+    0 // return an integer exit code
---
+++ Antilop/src/Library/Library.fs
@@ -0,0 +1,75 @@
+﻿namespace Library
+
+module ScalerCore =
+
+    type Note =
+        | C
+        | Cs
+        | D
+        | Ds
+        | E
+        | F
+        | Fs
+        | G
+        | Gs
+        | A
+        | Bb
+        | B
+
+    // keynote, harmonic, chordal or disharmonious
+    type Role =
+        | Key
+        | Harm
+        | Chor
+        | Dis
+
+    type Scale = Note array
+    type Schema = Role array
+    
+    let allNotes : Scale = [| C; Cs; D; Ds; E; F; Fs; G; Gs; A; Bb; B |]
+
+    let mayorSchema : Schema = [| Key; Dis; Harm; Dis; Chor; Harm; Dis; Chor; Dis; Harm; Dis; Harm |]
+
+    let permuteNotes (note : Note) (notes : Scale) =
+        let i = notes |> Array.findIndex (fun x -> x = note)
+        let len = notes.Length
+
+        notes
+        |> Array.permute (fun x -> ((len + x - i) % len))
+        
+    let filterScale (x : Role) =
+        match x with
+        | Key -> true
+        | Chor -> true
+        | Harm -> true
+        | Dis -> false
+
+    let getScale (note : Note) (schema : Schema) =
+        allNotes
+        |> permuteNotes note
+        |> Array.zip schema
+        |> Array.filter (fun n -> filterScale (fst n))
+        |> Array.map snd
+   
+    let getMayorScale (note : Note) =
+        getScale note mayorSchema
+
+    let getNoteFromString (name : string) : Note =
+        match name with
+        | "C" -> C
+        | "Cs" -> Cs
+        | "C#" -> Cs
+        | "D" -> D
+        | "Ds" -> Ds
+        | "D#" -> Ds
+        | "E" -> E
+        | "F" -> F
+        | "Fs" -> Fs
+        | "F#" -> Fs
+        | "G" -> G
+        | "Gs" -> Gs
+        | "G#" -> Gs
+        | "A" -> A
+        | "Bb" -> Bb
+        | "B" -> B
+        | _ -> C
